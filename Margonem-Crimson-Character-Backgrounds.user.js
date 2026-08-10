5e44f88d3ba61c55844d7a63345f855188da5079
PMateusz03 <pmateusz999@gmail.com>
2026-08-10 20:04:20 +0200

    Move left-side background subjects outward

--- Margonem-Crimson-Character-Backgrounds.user.js
+++ Margonem-Crimson-Character-Backgrounds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Margonem Crimson - Tła postaci
 // @namespace    https://github.com/bSlawek-03/Margonem-Crimson
-// @version      4.5.0
+// @version      4.6.0
 // @description  Panel do przypisywania RAW tła do postaci Margonem.
 // @author       Slawek
 // @match        https://*.margonem.pl/*
@@ -15,7 +15,7 @@
 (() => {
   'use strict';
 
-  const STORAGE_KEY = 'margonem-crimson-character-backgrounds-v7';
+  const STORAGE_KEY = 'margonem-crimson-character-backgrounds-v8';
 
   const characters = [
     { key: 'fourhead', label: 'Fourhead', level: '114', slot: 1 },
@@ -29,12 +29,12 @@
   const knownCharacters = characters.map((item) => item.key);
 
   const defaultUrls = {
-    fourhead: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-renegat-baulus-background.png?v=5',
-    twohead: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/crimson-archdemon-background.png?v=5',
-    onehawk: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-versus-zoons-background.png?v=5',
-    threebane: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-lowcz-driady-background.png?v=5',
-    fivefang: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-tezcatlipoca-background.png?v=5',
-    'po prostu slawek': 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-ice-king-background.png?v=5'
+    fourhead: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-renegat-baulus-background.png?v=6',
+    twohead: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/crimson-archdemon-background.png?v=6',
+    onehawk: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-versus-zoons-background.png?v=6',
+    threebane: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-lowcz-driady-background.png?v=6',
+    fivefang: 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-tezcatlipoca-background.png?v=6',
+    'po prostu slawek': 'https://raw.githubusercontent.com/bSlawek-03/Margonem-Crimson/main/assets/titan-ice-king-background.png?v=6'
   };
 
   const defaults = Object.fromEntries(characters.map((item) => [item.key, {
